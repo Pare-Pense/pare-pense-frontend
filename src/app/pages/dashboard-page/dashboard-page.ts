77698ed5471f25959e9855c7493f0c993ce2676f
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-05-29 12:24:15 -0300

    feat: pipe para formatar valores em real

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TabsModule } from 'primeng/tabs';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { SpeedDialModule } from 'primeng/speeddial';
 import { LucideUser, LucideBell, LucideTrendingUp, LucidePizza } from '@lucide/angular';
+import { FmtRealPipe } from '../../util/fmt-real-pipe';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -18,8 +19,18 @@ import { LucideUser, LucideBell, LucideTrendingUp, LucidePizza } from '@lucide/a
     LucideTrendingUp,
     LucidePizza,
     SpeedDialModule,
+    FmtRealPipe,
   ],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.css',
 })
-export class DashboardPage {}
+export class DashboardPage {
+  protected periodoIdx = signal(1);
+  protected valorDespesas = signal(8300);
+  protected valorReceitas = signal(500);
+  protected valorLimiteMensal = signal(10000);
+
+  setPeriodoIdx(idx: number) {
+    this.periodoIdx.set(idx);
+  }
+}
