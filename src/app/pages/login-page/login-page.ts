36d9435172e38756fa7e4eac95a33ff67baa2a20
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-06-17 22:15:32 -0300

    feat: integração na pagina de login

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { Form, FormsModule, NgForm } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputGroupModule } from 'primeng/inputgroup';
@@ -7,7 +7,9 @@ import { InputTextModule } from 'primeng/inputtext';
 import { LucideLockKeyhole, LucideMail, LucideWallet } from '@lucide/angular';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/auth-service';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-login-page',
@@ -23,11 +25,43 @@ import { RouterLink } from '@angular/router';
     LucideWallet,
     PasswordModule,
     RouterLink,
+    MessageModule,
   ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
   email: string = '';
   senha: string = '';
+  loading = signal(false);
+
+  onSubmit(form: NgForm) {
+    this.loading.set(true);
+    this.authService.login(this.email, this.senha).subscribe({
+      next: () => {
+        this.loading.set(false);
+        const redirect = this.route.snapshot.queryParams['redirect'] ?? '/home';
+        this.router.navigateByUrl(redirect);
+      },
+      error: (err) => {
+        this.loading.set(false);
+        const msg: string | undefined = err?.error?.erro;
+        if (msg === 'Dados inválidos') {
+          for (const detail of err.error.detalhes) {
+            if (form.controls[detail.campo])
+              form.controls[detail.campo].setErrors({ custom: detail.mensagem });
+          }
+        } else if (msg === 'Usuário não encontrado') {
+          form.controls['email'].setErrors({ custom: msg });
+        } else if (msg === 'Senha inválida') {
+          form.controls['senha'].setErrors({ custom: msg });
+        }
+        console.error('Erro ao fazer login!', msg);
+      },
+    });
+  }
 }
