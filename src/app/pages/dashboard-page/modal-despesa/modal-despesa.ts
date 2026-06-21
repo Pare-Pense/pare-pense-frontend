1e181c6c758e3637f63e9d4a311064528e69bf3d
alinebmr <alinebrito.maracaja@gmail.com>
2026-06-21 18:11:18 -0300

    fix: uptade input use

--- src/app/pages/dashboard-page/modal-despesa/modal-despesa.ts
+++ src/app/pages/dashboard-page/modal-despesa/modal-despesa.ts
@@ -1,4 +1,4 @@
-import { Component, Input, model, signal } from '@angular/core';
+import { Component, input, model} from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -33,7 +33,7 @@ export class ModalDespesa {
   protected categorias = ['Alimentação', 'Lazer', 'Transporte', 'Compras', 'Contas', 'Outros'];
 
   public isDespesa = model(true);
-  @Input() isEditMode = false;
+  isEditMode = input(false);
   protected valNome?: string;
   protected valCategoria?: string;
   protected valData?: Date;
