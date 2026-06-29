4a9ff99a17ca8317f9fdd10a7244e405c898dca5
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-06-29 17:44:08 -0300

    feat: redirecionar para login ao erro de token invalido

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, ToastModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('pare-pense-front');
-}
+export class App {}
