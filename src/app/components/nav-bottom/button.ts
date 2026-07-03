b1a2e35abfb834212a79d5d3f0e9d35fe0560ed4
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-07-03 14:44:51 -0300

    fix: mudar icones do nav bottom

--- src/app/components/nav-bottom/button.ts
+++ src/app/components/nav-bottom/button.ts
@@ -1,6 +1,11 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { Router } from '@angular/router';
-import { LucideHouse, LucideAtom, LucideGlasses, LucideDynamicIcon } from '@lucide/angular';
+import {
+  LucideHouse,
+  LucideDynamicIcon,
+  LucideReceiptText,
+  LucideCircleDollarSign,
+} from '@lucide/angular';
 
 @Component({
   selector: 'app-nav-bottom-button',
@@ -9,18 +14,18 @@ import { LucideHouse, LucideAtom, LucideGlasses, LucideDynamicIcon } from '@luci
 })
 export class NavBottomButton {
   public checked = input(false);
-  public icon = input.required<'house' | 'atom' | 'glasses'>();
+  public icon = input.required<'home' | 'despesas' | 'receitas'>();
   public url = input.required<string>();
   private router = inject(Router);
 
   protected iconProp = computed(() => {
     switch (this.icon()) {
-      case 'house':
+      case 'home':
         return LucideHouse;
-      case 'atom':
-        return LucideAtom;
-      case 'glasses':
-        return LucideGlasses;
+      case 'despesas':
+        return LucideReceiptText;
+      case 'receitas':
+        return LucideCircleDollarSign;
     }
   });
 
