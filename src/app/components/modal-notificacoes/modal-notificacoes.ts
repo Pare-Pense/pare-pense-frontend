9b9d669774121d04c9b67d7912da3ae5016f1d02
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-07-08 15:15:19 -0300

    feat: versão inicial modal de notificações

---
+++ src/app/components/modal-notificacoes/modal-notificacoes.ts
@@ -0,0 +1,50 @@
+import { Component, model } from '@angular/core';
+import { LucideBell, LucideX } from '@lucide/angular';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { FmtTimestampPipe } from '../../util/fmt-timestamp';
+
+@Component({
+  selector: 'app-modal-notificacoes',
+  imports: [
+    ButtonModule,
+    ProgressSpinnerModule,
+    DialogModule,
+    LucideBell,
+    LucideX,
+    FmtTimestampPipe,
+  ],
+  templateUrl: './modal-notificacoes.html',
+})
+export class ModalNotificacoes {
+  public visible = model(false);
+  notificacoes = [
+    {
+      id: 1,
+      msg: 'Isso é uma notificação de teste!',
+      lida: false,
+      timestamp: new Date(),
+    },
+    {
+      id: 2,
+      msg: 'Isso é uma notificação de teste! e contem ainda mais texto! texto texto texto texto',
+      lida: false,
+      timestamp: new Date(Date.now() - 2 * 3600 * 1000),
+    },
+    {
+      id: 3,
+      msg: 'Isso é uma notificação de teste! ainda bem que você já leu ela',
+      lida: true,
+      timestamp: new Date(Date.now() - 40 * 3600 * 1000),
+    },
+    ...Array(10)
+      .fill(0)
+      .map((_, i) => ({
+        id: 4 + i,
+        msg: 'Isso é uma notificação de teste!',
+        lida: i % 2 == 1,
+        timestamp: new Date(Date.now() - 89 * (i + 1) * 3600 * 1000),
+      })),
+  ];
+}
--- src/app/components/top-bar/top-bar.ts
+++ src/app/components/top-bar/top-bar.ts
@@ -1,13 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideBell, LucideUser } from '@lucide/angular';
 import { ButtonModule } from 'primeng/button';
+import { ModalNotificacoes } from '../modal-notificacoes/modal-notificacoes';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [ButtonModule, RouterLink, LucideBell, LucideUser],
+  imports: [ButtonModule, RouterLink, LucideBell, LucideUser, ModalNotificacoes],
   templateUrl: './top-bar.html',
 })
 export class TopBar {
   public title = input.required<string>();
+  modalNotifs = signal(false);
 }
