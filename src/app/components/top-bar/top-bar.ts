641121a0a46150c75c0f399fb9a87aa00e1334ce
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-07-11 16:34:26 -0300

    feat: bolha de notificação no topbar

--- src/app/components/top-bar/top-bar.ts
+++ src/app/components/top-bar/top-bar.ts
@@ -1,8 +1,12 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideBell, LucideUser } from '@lucide/angular';
 import { ButtonModule } from 'primeng/button';
 import { ModalNotificacoes } from '../modal-notificacoes/modal-notificacoes';
+import { AuthService } from '../../auth/auth-service';
+import { NotificacaoService } from '../../services/notificacao-service';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-top-bar',
@@ -10,6 +14,15 @@ import { ModalNotificacoes } from '../modal-notificacoes/modal-notificacoes';
   templateUrl: './top-bar.html',
 })
 export class TopBar {
+  private authService = inject(AuthService);
+  private notificacaoService = inject(NotificacaoService);
+
   public title = input.required<string>();
   modalNotifs = signal(false);
+
+  queryNotif = injectQuery(() => ({
+    queryKey: ['notificacoes', this.authService.getUsuarioId(), 'count'],
+    queryFn: () =>
+      lastValueFrom(this.notificacaoService.getNotificacaoCount(this.authService.getUsuarioId()!)),
+  }));
 }
