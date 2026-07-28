af9939c2f39f533c7f33f3433f95ac4a4a6f061a
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-07-28 13:14:32 -0300

    feat: fazer os testes cypress em viewport mobile

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    viewportWidth: 440,
+    viewportHeight: 850,
   },
 });
